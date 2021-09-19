fff3aa3e5e58d3c3b0dca27da4cace1a0dbb2057
Gokulraj T <64709230+gokulvm@users.noreply.github.com>
2021-09-19 13:32:46 +0530

    Add files via upload

---
+++ Basic_nodejs_modules/url_module.js
@@ -0,0 +1,20 @@
+// Reading the file by location
+// Enter path in url
+http_var = require("http") 
+fs = require('fs')
+url = require("url")
+http_var.createServer(function(req,res){
+    link = url.parse(req.url,true)
+    path_to_file = "."+link.pathname
+    fs.readFile(path_to_file,function(err,datas){
+        if (err){
+            res.writeHead(404,{"Content-type" : "text/html"})
+            res.end('404 - File not found')
+        }
+        res.writeHead(200,{"Content-type" : "text/html"});
+        res.write(datas);
+        res.end()
+
+    })
+   
+}).listen(8080) // Port number
