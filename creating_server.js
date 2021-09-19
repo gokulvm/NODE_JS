b3001b40ac1036a08bc55a92aae80d933d1861e3
Gokulraj T <64709230+gokulvm@users.noreply.github.com>
2021-09-19 13:24:46 +0530

    Delete creating_server.js

--- creating_server.js
+++
@@ -1,7 +0,0 @@
-http_var = require("http") // Getting HTTP module to create the server
-
-http_var.createServer(function(req,res){
-    res.writeHead(200,{"Content-type" : "text/html"});
-    res.write("Server is Healthy");
-    res.end()
-}).listen(8080) // Port number
